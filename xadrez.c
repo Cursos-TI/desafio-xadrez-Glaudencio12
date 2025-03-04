2c4cda57ed50d873fa05b372cc3246fc5ecedfd4
Glaudencio Costa <147957324+Glaudencio12@users.noreply.github.com>
2025-03-04 23:01:26 +0000

    Nível Novato: Movimentação inicial das peças.

--- xadrez.c
+++ xadrez.c
@@ -4,18 +4,45 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+// Função com a estrutura de repetição For utilizada para simular a movimentação da Torre para a direita.
+void moverTorre(int movimentos){
+    printf("\nMovendo Torre:\n");
+    for (int i = 1; i <= movimentos; i++){
+        printf("Direita --->  \n");
+    }
+}
+
+// Função com a estrutura de repetição Do-While utilizada para simular a movimentação da Rainha para a esquerda.
+void moverRainha(int movimentos){
+    printf("\nMovendo Rainha:\n");
+    do{
+        printf("<--- Esquerda\n");
+        movimentos--;
+    } while (movimentos >= 1);
+}
+
+// Função com a estrutura de repetição While utilizada para simular a movimentação do Bispo para a diagonal.
+void moverBispo(int movimentos){
+    printf("\nMovendo o Bispo:\n");
+    while (movimentos >= 1){
+        printf("Cima ^, Direita --->\n");
+        movimentos--;
+    }
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    // Declaração de variáveis constantes para representar o número de casas que cada peça pode se mover.
+    const int movimentosBispo = 5;
+    const int movimentoTorre = 5;
+    const int movimentoRainha = 8;
+    
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    moverBispo(movimentosBispo);
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    moverTorre(movimentoTorre);
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    moverRainha(movimentoRainha);
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
